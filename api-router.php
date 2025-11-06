c626d55e7222b0540af54d1b5a86b37eec49dd78
georgina <suarezgeoor@gmail.com>
2025-11-05 22:19:54 -0300

    Librerias y carpetas para el proyecto

---
+++ api-router.php
@@ -0,0 +1,9 @@
+<?php
+require_once './libs/router/router.php';
+
+require_once './libs/jwt/jwt.middleware.php';
+
+require_once './app/middlewares/guard-api.middleware.php';
+require_once './app/controllers/viaje-api.controller.php';
+require_once './app/controllers/auth-api.controller.php';
+
